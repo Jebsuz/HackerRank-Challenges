abe73c85d9d30d637636f315807a16e161f1bfc0
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 16:18:12 +0300

    solved algorithims/warmup#BirthdayCakeCandles challenge

--- algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/BirthdayCakeCandles.java
+++ algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/BirthdayCakeCandles.java
@@ -1,11 +1,16 @@
 package ru.jebsuz.hrc.algorithms.warmup;
 
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
 /**
  * https://www.hackerrank.com/challenges/birthday-cake-candles/problem
  */
 public class BirthdayCakeCandles {
 
   static int birthdayCakeCandles(int n, int[] ar) {
-    return 0;
+    final OptionalInt max = IntStream.of(ar).max();
+    final long result = IntStream.of(ar).filter(value -> value == max.getAsInt()).count();
+    return (int) result;
   }
 }
