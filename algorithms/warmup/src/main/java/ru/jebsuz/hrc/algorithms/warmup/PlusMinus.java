36bec0b51ea94fb2eb3d11230adb791f3b2a461d
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 15:01:28 +0300

    solved algorithims/warmup#PlusMinus challenge

--- algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/PlusMinus.java
+++ algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/PlusMinus.java
@@ -6,6 +6,24 @@ package ru.jebsuz.hrc.algorithms.warmup;
 public class PlusMinus {
 
   static void plusMinus(int[] arr) {
+    int positive = 0;
+    int negative = 0;
+    int zero = 0;
 
+    for (int i : arr) {
+      if (i > 0) {
+        positive++;
+      } else if (i < 0) {
+        negative++;
+      } else {
+        zero++;
+      }
+    }
+
+    final double positiveRatio = (double) positive / arr.length;
+    final double negativeRatio = (double) negative / arr.length;
+    final double zeroRatio = (double) zero / arr.length;
+
+    System.out.printf("%1.6f\n%2.6f\n%3.6f", positiveRatio, negativeRatio, zeroRatio);
   }
 }
