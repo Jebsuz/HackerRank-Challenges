0ea9e65819a0f95756ba218d30eb08b904101113
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 19:19:11 +0300

    solved hrc.ds.arrays#TwoDimArrayDS

--- data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/TwoDimArrayDS.java
+++ data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/TwoDimArrayDS.java
@@ -1,11 +1,41 @@
 package ru.jebsuz.hrc.ds.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://www.hackerrank.com/challenges/2d-array/problem
  */
 public class TwoDimArrayDS {
 
   static int array2D(int[][] arr) {
-    return 0;
+    List<Integer> results = new ArrayList<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (i + 3 > arr.length) {
+        continue;
+      }
+      for (int j = 0; j < arr[i].length; j++) {
+        if (j + 2 < arr[i].length) {
+          results.add(calc(arr, i, j));
+        }
+      }
+    }
+    return results.stream().mapToInt(Integer::valueOf).max().getAsInt();
+  }
+
+  private static int calc(int[][] arr, int height, int width) {
+    int sum = 0;
+    sum += calcRow(arr, height, width);
+    sum += arr[height + 1][width + 1];
+    sum += calcRow(arr, height + 2, width);
+    return sum;
+  }
+
+  private static int calcRow(int[][] arr, int height, int width) {
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+      sum += arr[height][width++];
+    }
+    return sum;
   }
 }
