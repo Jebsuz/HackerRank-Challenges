53bbc6b52f860a458a60b10ef573851406d5cfb4
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 14:30:51 +0300

    added algorithims/warmup#PlusMinus challenge

---
+++ algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/PlusMinus.java
@@ -0,0 +1,11 @@
+package ru.jebsuz.hrc.algorithms.warmup;
+
+/**
+ * https://www.hackerrank.com/challenges/plus-minus/problem
+ */
+public class PlusMinus {
+
+  static void plusMinus(int[] arr) {
+
+  }
+}
