808062233f5e350c55f7d27e1a84aae766d30d45
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 18:13:38 +0300

    added ds.arrays#ArraysDS test suite

---
+++ data-structures/arrays/src/test/java/ru/jebsuz/hrc/ds/arrays/ArraysDSTest.java
@@ -0,0 +1,55 @@
+package ru.jebsuz.hrc.ds.arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Collection;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import ru.jebsuz.hrc.common.SamplesLoader;
+
+@RunWith(Parameterized.class)
+public class ArraysDSTest extends BaseTest {
+
+  private int[] expected;
+  private int[] inputSample;
+
+  public ArraysDSTest(InputStream input, File output) throws Exception {
+    super(input, output);
+  }
+
+  @Parameters
+  public static Collection<Object[]> parameters() {
+    return SamplesLoader.load(ArraysDS.class.getSimpleName());
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    int arrCount = Integer.parseInt(input.nextLine().trim());
+
+    inputSample = new int[arrCount];
+
+    String[] arrItems = input.nextLine().split(" ");
+
+    for (int arrItr = 0; arrItr < arrCount; arrItr++) {
+      int arrItem = Integer.parseInt(arrItems[arrItr].trim());
+      inputSample[arrItr] = arrItem;
+    }
+
+    expected = new int[inputSample.length];
+    for (int i = 0; i < expected.length; i++) {
+      expected[i] = output.nextInt();
+    }
+  }
+
+  @Test
+  public void name() {
+    final int[] actual = ArraysDS.reverseArray(inputSample);
+
+    assertThat(actual).containsExactly(expected);
+  }
+}
