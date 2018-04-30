5eac33aaf40918aa47d96c5adf68a707e8daca73
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 15:29:14 +0300

    solved algorithims/warmup#Staircase challenge

--- algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/Staircase.java
+++ algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/Staircase.java
@@ -6,5 +6,19 @@ package ru.jebsuz.hrc.algorithms.warmup;
 public class Staircase {
 
   static void staircase(int n) {
+    StringBuilder level;
+    String emptySymbol = " ";
+    String stair = "#";
+    for (int i = 0; i < n; i++) {
+      level = new StringBuilder(n);
+      int numberOfStairsOnLevel = i + 1;
+      for (int j = 0; j < n - numberOfStairsOnLevel; j++) {
+        level.append(emptySymbol);
+      }
+      for (int k = 0; k < numberOfStairsOnLevel; k++) {
+        level.append(stair);
+      }
+      System.out.println(level);
+    }
   }
 }
