f4b6e90cec5d033aa352f061fe4efd7c7e7089a0
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 15:03:15 +0300

    added algorithims/warmup#Staircase challenge

---
+++ algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/Staircase.java
@@ -0,0 +1,10 @@
+package ru.jebsuz.hrc.algorithms.warmup;
+
+/**
+ * https://www.hackerrank.com/challenges/staircase/problem
+ */
+public class Staircase {
+
+  static void staircase(int n) {
+  }
+}
