0ba99313080b71c9ead4de52c688f650515bed0c
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 18:48:30 +0300

    added hrc.ds.arrays#TwoDimArrayDS challenge

---
+++ data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/TwoDimArrayDS.java
@@ -0,0 +1,11 @@
+package ru.jebsuz.hrc.ds.arrays;
+
+/**
+ * https://www.hackerrank.com/challenges/2d-array/problem
+ */
+public class TwoDimArrayDS {
+
+  static int array2D(int[][] arr) {
+    return 0;
+  }
+}
