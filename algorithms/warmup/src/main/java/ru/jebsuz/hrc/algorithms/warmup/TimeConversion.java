3cca7d26873069c0e414d0bcc6d139d461e02b15
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 16:23:39 +0300

    added algorithims/warmup#TimeConversion challenge

---
+++ algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/TimeConversion.java
@@ -0,0 +1,11 @@
+package ru.jebsuz.hrc.algorithms.warmup;
+
+/**
+ * https://www.hackerrank.com/challenges/time-conversion/problem
+ */
+public class TimeConversion {
+
+  static String timeConversion(String s) {
+    return "";
+  }
+}
