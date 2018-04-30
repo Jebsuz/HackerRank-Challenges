bd61dfc63eaf94e2001c4a5a3aab899198c35cfb
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 13:58:53 +0300

    added algorithims/warmup#DiagonalDifference challenge

---
+++ algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/DiagonalDifference.java
@@ -0,0 +1,11 @@
+package ru.jebsuz.hrc.algorithms.warmup;
+
+/**
+ * https://www.hackerrank.com/challenges/diagonal-difference/problem
+ */
+public class DiagonalDifference {
+
+  static int diagonalDifference(int[][] a) {
+    return 0;
+  }
+}
