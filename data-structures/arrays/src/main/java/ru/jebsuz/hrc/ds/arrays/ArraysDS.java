4420a0cb08c7dda575a823130a04c7b815f95afc
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 17:55:34 +0300

    added data-structures/arrays#ArrayDS challenge

---
+++ data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/ArraysDS.java
@@ -0,0 +1,11 @@
+package ru.jebsuz.hrc.ds.arrays;
+
+/**
+ * https://www.hackerrank.com/challenges/arrays-ds/problem
+ */
+public class ArraysDS {
+
+  static int[] reverseArray(int[] a) {
+    return null;
+  }
+}
