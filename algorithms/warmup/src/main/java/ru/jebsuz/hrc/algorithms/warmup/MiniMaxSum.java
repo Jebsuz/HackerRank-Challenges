cb59664bffac4203bcea747c2840a9d88e0d4da0
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 15:33:30 +0300

    added algorithims/warmup#MiniMaxSum challenge

---
+++ algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/MiniMaxSum.java
@@ -0,0 +1,10 @@
+package ru.jebsuz.hrc.algorithms.warmup;
+
+/**
+ * https://www.hackerrank.com/challenges/mini-max-sum/problem
+ */
+public class MiniMaxSum {
+
+  static void miniMaxSum(int[] arr) {
+  }
+}
