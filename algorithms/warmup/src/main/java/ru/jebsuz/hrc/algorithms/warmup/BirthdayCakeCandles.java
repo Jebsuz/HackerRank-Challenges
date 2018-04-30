967513d7cbaf3fb2b39a4f470d2ddbe0f47507d9
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-04-30 16:12:55 +0300

    added algorithims/warmup#BirthdayCakeCandles challenge

---
+++ algorithms/warmup/src/main/java/ru/jebsuz/hrc/algorithms/warmup/BirthdayCakeCandles.java
@@ -0,0 +1,11 @@
+package ru.jebsuz.hrc.algorithms.warmup;
+
+/**
+ * https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+ */
+public class BirthdayCakeCandles {
+
+  static int birthdayCakeCandles(int n, int[] ar) {
+    return 0;
+  }
+}
