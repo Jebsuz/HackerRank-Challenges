d00900d6d86831ad7a3160e6de452d36245263b8
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-05-04 23:56:07 +0300

    hrc.ds.arrays#DynamicArray got rid from custom SeqList class hrc.ds.arrays#DynamicArray replaced if's for queryType with switch hrc.ds.arrays#DynamicArray removed commented out code

--- data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/DynamicArray.java
+++ data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/DynamicArray.java
@@ -10,30 +10,27 @@ import java.util.List;
 public class DynamicArray {
 
   static int[] dynamicArray(int n, int[][] queries) {
-//    List<Integer> result = new ArrayList<>(9999999);
     int[] result = new int[50_000];
     int nextResultIndex = 0;
     int lastAnswer = 0;
-    SeqList[] sequences = new SeqList[n];
+    List<Integer>[] sequences = new LinkedList[n];
     for (int i = 0; i < n; i++) {
-      sequences[i] = new SeqList(n);
+      sequences[i] = new LinkedList<>();
     }
     for (int[] query : queries) {
       int queryType = query[0];
       int x = query[1];
       int y = query[2];
       int sequenceIndex = getIndex(x, lastAnswer, n);
-      if (queryType == 1) {
-//        try {
-        sequences[sequenceIndex].add(y);
-//        } catch (Exception e) {
-//          System.out.println(queryType + " : " + x + " : " + y);
-//        }
-      }
-      if (queryType == 2) {
-        SeqList sequence = sequences[sequenceIndex];
-        lastAnswer = sequence.get(y % sequence.size());
-        result[nextResultIndex++] = lastAnswer;
+      switch (queryType) {
+        case 1:
+          sequences[sequenceIndex].add(y);
+          break;
+        case 2:
+          List<Integer> sequence = sequences[sequenceIndex];
+          lastAnswer = sequence.get(y % sequence.size());
+          result[nextResultIndex++] = lastAnswer;
+          break;
       }
     }
     return Arrays.copyOfRange(result, 0, nextResultIndex);
@@ -41,34 +38,5 @@ public class DynamicArray {
 
   private static int getIndex(int first, int second, int size) {
     return (first ^ second) % size;
-  }
-
-  static class SeqList {
-
-    //    int[] store;
-    List<Integer> store;
-    int next;
-    private int size = 0;
-
-    public SeqList(int size) {
-//      store = new int[size];
-      store = new LinkedList<>();
-      next = 0;
-    }
-
-    void add(int value) {
-      store.add(value);
-      size++;
-//      store[next++ % store.length] = value;
-    }
-
-    int get(int index) {
-//      return store[index];
-      return store.get(index);
-    }
-
-    int size() {
-      return size;
-    }
   }
 }
