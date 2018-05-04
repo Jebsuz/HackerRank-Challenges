f64711eef96fcfb1c5d51ee2a39dc01e132eee93
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-05-05 00:00:43 +0300

    hrc.ds.arrays#DynamicArray replaced int[] for results with LinkedList<Integer>

--- data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/DynamicArray.java
+++ data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/DynamicArray.java
@@ -1,6 +1,5 @@
 package ru.jebsuz.hrc.ds.arrays;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,7 +9,7 @@ import java.util.List;
 public class DynamicArray {
 
   static int[] dynamicArray(int n, int[][] queries) {
-    int[] result = new int[50_000];
+    List<Integer> result = new LinkedList<>();
     int nextResultIndex = 0;
     int lastAnswer = 0;
     List<Integer>[] sequences = new LinkedList[n];
@@ -29,11 +28,11 @@ public class DynamicArray {
         case 2:
           List<Integer> sequence = sequences[sequenceIndex];
           lastAnswer = sequence.get(y % sequence.size());
-          result[nextResultIndex++] = lastAnswer;
+          result.add(lastAnswer);
           break;
       }
     }
-    return Arrays.copyOfRange(result, 0, nextResultIndex);
+    return result.stream().mapToInt(Integer::valueOf).toArray();
   }
 
   private static int getIndex(int first, int second, int size) {
