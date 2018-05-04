4df9e233b937a898609c2aa0f0f4ae7ac1dd203f
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-05-05 01:23:44 +0300

    added hrc.ds.arrays#ArrayManipulation challenge

---
+++ data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/ArrayManipulation.java
@@ -0,0 +1,11 @@
+package ru.jebsuz.hrc.ds.arrays;
+
+/**
+ * https://www.hackerrank.com/challenges/crush/problem
+ */
+public class ArrayManipulation {
+
+  static int arrayManipulation(int n, int[][] queries) {
+    return 0;
+  }
+}
---
+++ data-structures/arrays/src/test/java/ru/jebsuz/hrc/ds/arrays/ArrayManipulationTest.java
@@ -0,0 +1,58 @@
+package ru.jebsuz.hrc.ds.arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+import java.util.Scanner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import ru.jebsuz.hrc.common.SamplesLoader;
+
+@RunWith(Parameterized.class)
+public class ArrayManipulationTest extends BaseTest {
+
+  private int[][] queries;
+  private int listSize;
+  private int expected;
+
+  public ArrayManipulationTest(Scanner input, Scanner output) {
+    super(input, output);
+  }
+
+  @Parameters
+  public static Collection<Object[]> parameters() {
+    return SamplesLoader.load(ArrayManipulation.class);
+  }
+
+  @Before
+  public void setUp() {
+    String[] nm = input.nextLine().split(" ");
+
+    listSize = Integer.parseInt(nm[0].trim());
+
+    int numberOfOperations = Integer.parseInt(nm[1].trim());
+
+    queries = new int[numberOfOperations][3];
+
+    for (int queriesRowItr = 0; queriesRowItr < numberOfOperations; queriesRowItr++) {
+      String[] queriesRowItems = input.nextLine().split(" ");
+
+      for (int queriesColumnItr = 0; queriesColumnItr < 3; queriesColumnItr++) {
+        int queriesItem = Integer.parseInt(queriesRowItems[queriesColumnItr].trim());
+        queries[queriesRowItr][queriesColumnItr] = queriesItem;
+      }
+    }
+
+    expected = output.nextInt();
+  }
+
+  @Test
+  public void arrayManipulation() {
+    int actual = ArrayManipulation.arrayManipulation(listSize, queries);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+}
