17300c55a4515d9a27d0e06100ca6eca99ca4c69
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-05-04 22:04:33 +0300

    added hrc.ds.arrays#DynamicArray challenge

---
+++ data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/DynamicArray.java
@@ -0,0 +1,11 @@
+package ru.jebsuz.hrc.ds.arrays;
+
+/**
+ * https://www.hackerrank.com/challenges/dynamic-array/problem
+ */
+public class DynamicArray {
+
+  static int[] dynamicArray(int n, int[][] queries) {
+    return null;
+  }
+}
