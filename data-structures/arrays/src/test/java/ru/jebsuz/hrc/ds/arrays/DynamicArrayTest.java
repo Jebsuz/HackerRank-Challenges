d6151f086905c4cf9220ef4b92c43c0053579c6d
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-05-04 23:40:20 +0300

    added more test data for for hrc.ds.arrays#DynamicArray removed unused import in hrc.ds.arrays#DynamicArrayTest

--- data-structures/arrays/src/test/java/ru/jebsuz/hrc/ds/arrays/DynamicArrayTest.java
+++ data-structures/arrays/src/test/java/ru/jebsuz/hrc/ds/arrays/DynamicArrayTest.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
