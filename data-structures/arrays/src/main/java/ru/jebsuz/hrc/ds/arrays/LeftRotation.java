264a2e0d300ab7a8aad3167eb6829f729b6509e6
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-05-05 00:36:58 +0300

    solved hrc.ds.arrays#LeftRotation

--- data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/LeftRotation.java
+++ data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/LeftRotation.java
@@ -1,11 +1,21 @@
 package ru.jebsuz.hrc.ds.arrays;
 
+import java.util.Arrays;
+
 /**
  * https://www.hackerrank.com/challenges/array-left-rotation/problem
  */
 public class LeftRotation {
 
   static int[] rotateLeft(int[] array, int times) {
-    return null;
+    int[] result = new int[array.length];
+    int rotationIndex = times % array.length;
+    System.arraycopy(array, rotationIndex, result, 0, array.length - rotationIndex);
+    System.arraycopy(array, 0, result, array.length - rotationIndex, rotationIndex);
+    return result;
+  }
+
+  static void printResult(int[] result) {
+    Arrays.stream(result).boxed().forEachOrdered(integer -> System.out.print(integer + " "));
   }
 }
