16bbd03f3960cde9e0a40274cab5e20b77369f78
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-05-05 21:27:28 +0300

    solved hrc.ds.arrays#SparseArrays challenge

--- data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/SparseArrays.java
+++ data-structures/arrays/src/main/java/ru/jebsuz/hrc/ds/arrays/SparseArrays.java
@@ -1,11 +1,18 @@
 package ru.jebsuz.hrc.ds.arrays;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * https://www.hackerrank.com/challenges/sparse-arrays/problem
  */
 public class SparseArrays {
 
   static int findSuffix(String[] collections, String queryString) {
-    return 0;
+    Map<String, Integer> occurrences = Arrays.stream(collections)
+        .collect(Collectors.toMap(Function.identity(), s -> 1, (a, b) -> a + b));
+    return occurrences.getOrDefault(queryString, 0);
   }
 }
