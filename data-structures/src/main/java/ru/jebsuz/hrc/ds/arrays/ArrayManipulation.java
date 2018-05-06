d9d03a01cbae2671d5df732b138d85dc3ad9fede
Maksim Starshinov <5747696+Jebsuz@users.noreply.github.com>
2018-05-06 22:32:19 +0300

    merged module data-structures#Arrays to data-structures, will split submodules on package level moved dependency for common library to root pom

--- data-structures/src/main/java/ru/jebsuz/hrc/ds/arrays/ArrayManipulation.java
+++ data-structures/src/main/java/ru/jebsuz/hrc/ds/arrays/ArrayManipulation.java
@@ -1,9 +1,6 @@
 package ru.jebsuz.hrc.ds.arrays;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * https://www.hackerrank.com/challenges/crush/problem
